880b0a764a6b0b0dc595aa5c539916713340482f
Usman Jamil <usman.jamil@live.co.uk>
2015-09-21 22:48:11 +0100

    end of day Monday

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -4,7 +4,10 @@ feature 'Viewing links' do
 
     scenario 'I can see existing links on the links page' do
       Link.new(url: 'http://www.makersacademy.com', title: 'Makers Academy')
-
+      visit '/links/new_links'
+      fill_in 'url',   with: 'http://www.makersacademy.com'
+      fill_in 'title', with: 'Makers Academy'
+      click_button 'Create link'
       visit '/links'
       expect(page.status_code).to eq 200
       within 'ul#links' do
