a3c5d24ddcedaa9a6a6bfe9bc71f399b0323a8ef
Usman Jamil <usman.jamil@live.co.uk>
2015-09-22 16:15:25 +0100

    added multiple tags rspec test but not passing

--- app/app.rb
+++ app/app.rb
@@ -16,8 +16,17 @@ class Bookmark_manager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.create(url: params[:url], title: params[:title])
+    p tag  = Tag.create(name: params[:tag])
+    p link.tags << tag
+    link.save
     redirect to('/links')
+  end
+
+  get '/tags/:name' do
+    p tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
   end
 
   run! if app_file == Bookmark_manager
