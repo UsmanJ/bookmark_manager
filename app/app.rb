50362eadeedf4c8df4ec008f161adc687362d018
Usman Jamil <usman.jamil@live.co.uk>
2015-09-23 18:01:58 +0100

    added test for preventing duplicate registrations but not passing test

--- app/app.rb
+++ app/app.rb
@@ -42,14 +42,14 @@ class Bookmark_manager < Sinatra::Base
   end
 
   post '/users' do
-    @user = User.create(email: params[:email],
+    @user = User.new(email: params[:email],
                        password: params[:password],
                        password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
       redirect to('/links')
     else
-      flash.now[:notice] = "Password and confirmation password do not match"
+      flash.now[:errors] = @user.errors.full_messages
       erb :'users/new'
     end
   end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -13,11 +13,17 @@ feature 'User sign up' do
   scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'assword does not match the confirmation'
   end
 
   scenario 'only if they provide an email address' do
     expect { sign_up(email: '') }.not_to change(User, :count)
+  end
+
+  scenario 'I cannot sign up with an existing email' do
+    sign_up
+    expect { sign_up }.to change(User, :count).by(0)
+    expect(page).to have_content('Email is already taken')
   end
 
   def sign_up(email: 'alice@example.com',
