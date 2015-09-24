646cc390f247e86c2a23854d5500db2a1493f433
Usman Jamil <usman.jamil@live.co.uk>
2015-09-24 12:02:35 +0100

    Factory Girl gem working

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -33,16 +33,6 @@ feature 'User sign up' do
     expect(page).to have_content('Email is already taken')
   end
 
-  def sign_up(email: 'alice@example.com',
-            password: '12345678',
-            password_confirmation: '12345678')
-    visit '/users/new'
-    fill_in :email, with: email
-    fill_in :password, with: password
-    fill_in :password_confirmation, with: password_confirmation
-    click_button 'Sign up'
-  end
-
   def sign_up2(user)
     visit '/users/new'
     fill_in :email, with: user.email
